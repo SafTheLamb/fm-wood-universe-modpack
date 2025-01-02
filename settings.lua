f41f8d06abcac386e8f4d5297659823b8fa5c4df
sapphiriza <sapphiriza@gmail.com>
2025-01-01 18:53:18 -0800

    Version 0.2.0: Adding Tin! Promoting other mods to required and adding new optional suggestions.

--- settings.lua
+++ settings.lua
@@ -1,1 +1,5 @@
 data.raw["bool-setting"]["wood-military-hard-mode"].default_value = true
+data.raw["string-setting"]["bztin-more-intermediates"].default_value = "cable"
+if mods["aai-loaders"] then
+  data.raw["string-setting"]["aai-loaders-lubricant-recipe"].default_value = "enabled"
+end
