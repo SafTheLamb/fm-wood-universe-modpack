caa9eb3e09289f95056b92bbd03e1ed41d170c25
sapphiriza <sapphiriza@gmail.com>
2025-02-21 20:00:44 -0800

    Version 0.4.0: Scrappy Industry Edition.

--- settings.lua
+++ settings.lua
@@ -1,5 +1,7 @@
 data.raw["bool-setting"]["wood-military-hard-mode"].default_value = true
-data.raw["string-setting"]["bztin-more-intermediates"].default_value = "cable"
+if mods["bztin"] then
+  data.raw["string-setting"]["bztin-more-intermediates"].default_value = "cable"
+end
 if mods["aai-loaders"] then
   data.raw["string-setting"]["aai-loaders-lubricant-recipe"].default_value = "enabled"
 end
