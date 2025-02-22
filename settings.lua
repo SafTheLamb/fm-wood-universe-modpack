05b28af1daedac7fcd0c88c246a488ce79af9bec
sapphiriza <sapphiriza@gmail.com>
2025-02-21 21:04:01 -0800

    Version 0.4.1 and Version 0.4.2: Frost biters and Diversitrees.

--- settings.lua
+++ settings.lua
@@ -1,4 +1,6 @@
 data.raw["bool-setting"]["wood-military-hard-mode"].default_value = true
+data.raw["bool-setting"]["cb-enable-cold-warfare"].default_value = false
+data.raw["string-setting"]["fb-spawn-planet"].default_value = "aquilo"
 if mods["bztin"] then
   data.raw["string-setting"]["bztin-more-intermediates"].default_value = "cable"
 end
