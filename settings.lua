5dd8e6081fd17aa2aa0f17329955c0b3f56489c5
sapphiriza <sapphiriza@gmail.com>
2025-04-29 13:49:29 -0700

    Dependency changes

--- settings.lua
+++ settings.lua
@@ -5,7 +5,12 @@ local function change_default_value(setting_type, setting_name, default_value)
   end
 end
 
+change_default_value("string-setting", "aai-loaders-lubricant-recipe", "enabled")
+
+change_default_value("string-setting", "bztin-more-intermediates", "cable")
+
 change_default_value("bool-setting", "cb-enable-cold-warfare", false)
 change_default_value("string-setting", "fb-spawn-planet", "aquilo")
-change_default_value("string-setting", "bztin-more-intermediates", "cable")
-change_default_value("string-setting", "aai-loaders-lubricant-recipe", "enabled")
+
+change_default_value("string-setting", "visible-pollutants-add-to-planets", "vulcanus")
+change_default_value("double-setting", "visible-pollutants-opacity", 0.5)
